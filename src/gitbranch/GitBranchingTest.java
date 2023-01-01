6f02f5ff7ed557fc43e54034860b5015ff45e642
Noman Bardasht <noman.bardasht@tekschool.us>
2023-01-01 00:35:51 -0500

    Capstone

--- src/gitbranch/GitBranchingTest.java
+++ src/gitbranch/GitBranchingTest.java
@@ -5,6 +5,11 @@ public class GitBranchingTest {
 	public static void main(String[] args) {
 		
 		System.out.println("Git Branch Testing is on the way");
+		
+		public void thisIsFunWhenItWorks() {
+			
+		System.out.println("this is fun");
+		
 
 	}
 
